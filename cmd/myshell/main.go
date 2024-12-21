8aa2ec69da5635248750f7fa0d7a6fe0c963e661
marlonpassos <marlon.passos@cadastra.com>
2024-12-21 04:10:49 +0000

    feat: implementa repl

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -13,19 +13,23 @@ var _ = fmt.Fprint
 var validCommands = []string{}
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	repl()
+}
 
-	// Wait for user input
+func repl() {
+	fmt.Fprint(os.Stdout, "$ ")
 	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	trimmedCommand := strings.TrimSpace(command)
 	if err != nil {
-		fmt.Println("Error reading input")
+		fmt.Fprintln(os.Stderr, "Error reading input")
 		os.Exit(1)
 	}
 
 	if !contains(validCommands, trimmedCommand) {
 		fmt.Fprintf(os.Stderr, "%s: command not found\n", trimmedCommand)
 	}
+
+	repl()
 }
 
 func contains(arr []string, str string) bool {
